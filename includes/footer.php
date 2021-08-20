a2fc253965e5d9b101068018ba638ade25f23f2b
epixeltechnologies <30853858+epixeltechnologies@users.noreply.github.com>
2021-08-20 16:43:54 +0530

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('table').DataTable();
+		});
+	</script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script type="text/javascript" src="./assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
+</body>
+</html>
